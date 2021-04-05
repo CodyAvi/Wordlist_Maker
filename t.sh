4c8017a660f10feea84beb143b086946e15f2bb5
Aviral Srivastava <79373546+avistnm@users.noreply.github.com>
2021-04-05 11:38:00 +0530

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,58 +1,55 @@
 k=8
-i=0
 m=""
 p=0
+numo=16
+i=10
 
-declare -a c
-for((i=0;i<k;++i))
+for ((i=0;i<k;++i ))
 do
-c[i]=$(chr 33)
+d=$(chr 33)
+${c[i]}=$d
 done
 
-while($k<16)
+while [ $k -lt 16 ]
 do
-if($k==8)
-then
-for((i=0;i<k;++i))
+for ((i=0;i<255;++i))
 do
-$m+=${c[i]}
-echo $m>>lop.txt
+n=$m${c[i]}
+echo $n>>lop.txt
 done
-fi
 
-for((i=0;i<k;++i))
+for ((i=0;i<k;++i))
 do
 d=$(chr 255)
-if(${c[i]}!=$d)
+if [ ${c[i]} != "$d" ]
 then
 ++p
 fi
 done
 
-if($p==0)
+if [ $p -eq 0 ]
 then
-for((i=0;i<k;++i))
+for ((i=0;i<k;++i))
 do
 c[i]=$(chr 33)
-++k
+k=$k+1;
 done
 fi
 m="";
 
-for((i=(k-1);i>0;--i))
+for ((i=(k-1);i>0;--i))
 do
-if(${c[i]}==$(chr 255))
+if [ ${c[i]} == "$(chr 255)" ]
 then
 d=$(chr 33)
 c[i]=$d
 fi
-if(${c[i-1]}==$(chr 255) && i>0)
+if [ ${c[i-1]} == "$(chr 255)" & $i -gt 0 ]
 then
 continue;
-++c[i-1];
+++${c[i-1]};
+else ++${c[i]}; break;
 fi
-else ++c[i]; break;
 done
 
-fi
 done
