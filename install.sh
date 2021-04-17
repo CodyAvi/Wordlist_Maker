be3296c0dc850674b250ed6849d95505d127bf1d
Aviral Srivastava <79373546+avistnm@users.noreply.github.com>
2021-04-17 22:13:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-cd $PREFIX/bin
-rm -rf chr
-rm -rf rchr
-cd $HOME
-rm -rf r.sh
-mkdir tempbin
-curl -LO https://github.com/avistnm/test/blob/master/chr
-curl -LO https://github.com/avistnm/test/blob/master/rchr
-mv * $PREFIX/bin
-curl -LO https://github.com/avistnm/test/blob/master/r.sh
-chmod 777 r.sh
-mv r.sh $HOME
-cd ..
-rm -rf tempbin
+echo -e "\e[34m Installing core files.....\e[0m"
+cd $PREFIX/bin >/dev/null 2>&1
+rm -rf chr >/dev/null 2>&1
+rm -rf rchr >/dev/null 2>&1
+cd $HOME >/dev/null 2>&1
+rm -rf r.sh >/dev/null 2>&1
+mkdir tempbin >/dev/null 2>&1
+wget https://raw.githubusercontent.com/avistnm/test/master/chr >/dev/null 2>&1
+wget https://raw.githubusercontent.com/avistnm/test/master/rchr >/dev/null 2>&1
+mv * $PREFIX/bin >/dev/null 2>&1
+wget https://raw.githubusercontent.com/avistnm/test/master/r.sh >/dev/null 2>&1
+chmod 777 r.sh >/dev/null 2>&1
+mv r.sh $HOME >/dev/null 2>&1
+cd .. >/dev/null 2>&1
+rm -rf tempbin >/dev/null 2>&1
+echo -e "\e[34m Installation Successful!!\e[0m"
+echo -e "\e[34m Run Script by executing \e[1;32bash r.sh\e[0m"
